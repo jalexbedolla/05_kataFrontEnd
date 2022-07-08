72bf7ac4f4d10648a4663bf90fc2b9f870320e8e
alex Bedolla <j.alexbedolla.a@gmail.com>
2022-07-08 11:39:13 -0600

    haciendo cambios pendejos

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hola Perro..!!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React.. y se Feliz..!!
-          </a>
-        </header>
       </div>
     );
   }
