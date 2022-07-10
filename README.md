a4ad256e649a8bfae279db7e92d83fff5eaeb7a0
alex Bedolla <j.alexbedolla.a@gmail.com>
2022-07-09 23:56:49 -0600

    this.state getState and bind

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ esta es mi primera aplicasion echa con react create component y cuenta hacia arr
 ## comandos disponible
 ### `npm start`
 
+el "bind" tienen que ir dentro del "constructor" y las "funciones" afuera.. es como cuando mandas llamar una funcion dentro de la otra funcion en "js" un "bind" para cada "state"
+
--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export class App extends Component {
+  //funcion especial para inicializar nuestro objeto que es un componente de react
+  constructor(){
+    super()
+    this.state={
+      contador:0
+    }
+    this.aumentarContador = this.aumentarContador.bind(this)
+    this.restarContador = this.restarContador.bind(this)
+    //this.resetContador = this.resetContador.bind(this) no la usamos porque usamos una "arrow fuction"
+  }
+  // esta sintaxis es porque estamos usando "class extend component" mas adelante ya no las necesitaremos cuando utilisemos "fuctional component"
+  aumentarContador(){
+    if(this.state.contador < 15)
+    this.setState({ contador : this.state.contador + 1})
+  }
+
+  restarContador(){
+    if(this.state.contador > 0)
+    this.setState({ contador : this.state.contador - 1})
+  }
+  resetState(){
+    this.setState({ contador : 0})
+  }
+
   render() {
+    //let valor= 5
     return (
       <div className="App App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hola Perro..!!</p>
+          <p>aplicacion de Contador</p>
+          <p>
+            { this.state.contador }
+          </p>
+          <button onClick={ this.aumentarContador }>Up</button>
+          <button onClick={ this.restarContador }>Down</button>
+          <button onClick={ () => this.resetState() }>Reset</button>
       </div>
     );
   }
