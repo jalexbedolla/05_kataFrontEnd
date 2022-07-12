3d195f3cc3ba8f8d60e630e160b1fc73d3db5b5d
alex Bedolla <j.alexbedolla.a@gmail.com>
2022-07-11 23:24:23 -0600

    agregando limites

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export class App extends Component {
   constructor(){
     super()
     this.state={
-      contador:0
+      contador:0,
+      limiteInferior:0,
+      limiteSuperior:12
     }
     this.aumentarContador = this.aumentarContador.bind(this)
     this.restarContador = this.restarContador.bind(this)
@@ -15,16 +17,25 @@ export class App extends Component {
   }
   // esta sintaxis es porque estamos usando "class extend component" mas adelante ya no las necesitaremos cuando utilisemos "fuctional component"
   aumentarContador(){
-    if(this.state.contador < 15)
+    if(this.state.contador < 21)
     this.setState({ contador : this.state.contador + 1})
   }
 
   restarContador(){
-    if(this.state.contador > 0)
+    if(this.state.contador > this.state.limiteInferior)
     this.setState({ contador : this.state.contador - 1})
   }
+
   resetState(){
-    this.setState({ contador : 0})
+    this.setState({ contador : 0})    
+  }
+
+  establecerLimiteSuperior(nuevoLimite){// argumento.. que no se te olvide
+    this.setState({ limiteSuperior:nuevoLimite })
+  }
+
+  establecerLimiteInferiro(nuevoLimite){
+    this.setState({ limiteInferior:nuevoLimite })
   }
 
   render() {
@@ -33,11 +44,16 @@ export class App extends Component {
       <div className="App App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>aplicacion de Contador</p>
-          <p>
-            { this.state.contador }
-          </p>
-          <button onClick={ this.aumentarContador }>Up</button>
-          <button onClick={ this.restarContador }>Down</button>
+          <p>{ this.state.contador }</p>
+          <p>{JSON.stringify(this.state)}</p>{/* esto es para imprimir el estado */}
+          {/* button mas */}
+          <button onClick={ this.aumentarContador }>Mas</button>
+          <label>Limite Superior</label>
+          <input onChange={ (e) => this.establecerLimiteSuperior(e.target.value) } placeholder="limite Superior"/>
+          {/* button menos */}
+          <button onClick={ this.restarContador }>Menos</button>
+          <label>Limite Inferior</label>
+          <input onChange={ (e) => this.establecerLimiteInferior(e.target.value) } placeholder="limite inferior"/>
           <button onClick={ () => this.resetState() }>Reset</button>
       </div>
     );
